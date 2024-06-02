c953358a2ccb0bbadb7810b33e1c65ab3f80ab17
Yeyver <yeiver.cury@udea.edu.co>
2024-06-02 18:23:46 -0500

    actualizando el servidor

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const config = require('./config');
 
+const clientes = require('./modulos/usuarios/rutas')
+
 const app = express();
 
 //Configuración
 app.set('port', config.app.port)
+
+//rutas
+app.use('/usuarios', clientes)
 
 module.exports = app
